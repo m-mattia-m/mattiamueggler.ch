c34fa0ceb897a57be3b2523da0a85e857daca1fe
m-mattia-m <mattia@mattiamueggler.ch>
2024-07-05 20:08:20 +0200

    add assets and public RSA key

---
+++ error.vue
@@ -0,0 +1,22 @@
+<script setup>
+const error = useError();
+const properties = defineProps({
+  error: Object,
+});
+
+const showErrorPage = ref(false)
+
+onMounted(() => {
+  if (properties.error.statusCode === 404) navigateTo("/")
+  showErrorPage.value = true
+})
+
+</script>
+
+<template>
+    <div v-if="showErrorPage">
+      <h1>Error:</h1>
+      <p>It looks like something broke. - Sorry about that.</p>
+      <p>{{ error.statusCode }} - {{ error.message }}</p>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="w-full h-screen bg-[#90E0EF]">
+    <div class="w-full h-full flex items-center justify-center">
+      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+        <div class="flex justify-center items-center">
+          <div
+              class="bg-[#134074] text-white p-8 md:p-8 border rounded-lg transform transition ease-in duration-300 hover:scale-125">
+            <NuxtLink :external="true" to="https://cv.mattiamueggler.ch" target="_blank" @click="useTrackEvent('cv')">
+              <CV :fontControlled="false" :filled="true" class="h-14 w-14 text-white"/>
+            </NuxtLink>
+          </div>
+        </div>
+        <div class="flex justify-center items-center">
+          <div
+              class="bg-[#134074] text-white p-8 md:p-8 border rounded-lg transform transition ease-in duration-300 hover:scale-125">
+            <NuxtLink :external="true" to="https://github.com/m-mattia-m" target="_blank" @click="useTrackEvent('github')">
+              <Github :fontControlled="false" :filled="true" class="h-14 w-14"/>
+            </NuxtLink>
+          </div>
+        </div>
+        <div class="flex justify-center items-center">
+          <div
+              class="bg-[#134074] text-white p-8 md:p-8 border rounded-lg transform transition ease-in duration-300 hover:scale-125">
+            <NuxtLink :external="true" to="https://www.linkedin.com/in/mattia-mueggler" target="_blank" @click="useTrackEvent('linkedin')">
+              <Linkedin :fontControlled="false" :filled="true" class="h-14 w-14"/>
+            </NuxtLink>
+          </div>
+        </div>
+        <div class="flex justify-center items-center">
+          <div
+              class="bg-[#134074] text-white p-8 md:p-8 border rounded-lg transform transition ease-in duration-300 hover:scale-125">
+            <NuxtLink :external="true" to="https://bento.me/mattiamueggler" target="_blank" @click="useTrackEvent('bento')">
+              <Bento :fontControlled="false" :filled="true" class="h-14 w-14"/>
+            </NuxtLink>
+          </div>
+        </div>
+        <div class="flex justify-center items-center">
+          <div
+              class="bg-[#134074] text-white p-8 md:p-8 border rounded-lg transform transition ease-in duration-300 hover:scale-125">
+            <NuxtLink :external="true" to="https://github.com/m-mattia-m/notify/" target="_blank" @click="useTrackEvent('notify')">
+              <Bars :fontControlled="false" :filled="true" class="h-14 w-14"/>
+            </NuxtLink>
+          </div>
+        </div>
+        <div class="flex justify-center items-center">
+          <div
+              class="bg-[#134074] text-white p-8 md:p-8 border rounded-lg transform transition ease-in duration-300 hover:scale-125">
+            <NuxtLink :external="true" to="https://formtion.app" target="_blank" @click="useTrackEvent('formtion')">
+              <Formtion :fontControlled="false" :filled="true" class="h-14 w-14"/>
+            </NuxtLink>
+          </div>
+        </div>
+        <div class="flex justify-center items-center">
+          <div
+              class="bg-[#134074] text-white p-8 md:p-8 border rounded-lg transform transition ease-in duration-300 hover:scale-125">
+            <NuxtLink to="/assets" @click="useTrackEvent('info')">
+              <Info :fontControlled="false" :filled="true" class="h-14 w-14"/>
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+    <NuxtLink to="mailto:hi@mattiamueggler.ch"
+       class="absolute flex justify-center items-center w-full text-[#134074] bg-[#90E0EF] py-4 mt-[-60px] text-center">hi@mattiamueggler.ch</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import CV from "~/public/icons/cv.svg";
+import Github from "~/public/icons/github.svg";
+import Linkedin from "~/public/icons/linkedin.svg";
+import Bento from "~/public/icons/bento.svg";
+import Bars from "~/public/icons/bars.svg";
+import Formtion from "~/public/icons/formtion.svg";
+import Info from "~/public/icons/info.svg";
+</script>
